0a7f545c63361f3f15c9b05823f8f5b89915a9ab
Amir <firstharrypotter1@gmail.com>
2022-12-20 00:35:50 +0600

    solved first task

--- zad1/Program.cs
+++ zad1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().NextDouble() * 10;
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            double round = Math.Round(array[i, j], 2);
+            Console.Write($"{round,3}\t");
+        }
+        Console.WriteLine();
+    }   
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+double[,] table = new double[rows, cols];
+FillArray(table);
+PrintArray(table);
